bb666eb0f739d47e43c24490a336d8c7ad707d51
Jose_Varela <jo.lu6853@gmail.com>
2018-05-11 00:10:17 -0500

    Updates on graphics and positions

--- Scripts/untitled.lua
+++ Scripts/untitled.lua
@@ -5,14 +5,14 @@ function LifebarDoublesCheck(pn)
         if GAMESTATE:PlayerUsingBothSides() then
             return SCREEN_CENTER_X
         else
-            return SCREEN_CENTER_X-150
+            return SCREEN_CENTER_X+(160-320)
         end
     end
     if pn == PLAYER_2 then
         if GAMESTATE:PlayerUsingBothSides() then
             return SCREEN_CENTER_X
         else
-            return SCREEN_CENTER_X+150
+            return SCREEN_CENTER_X+(478-320)
         end
     end
 end
